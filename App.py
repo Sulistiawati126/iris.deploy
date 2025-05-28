6a52e20362d9f0c5b0d12cc4920799686ae510a9
Sulistiawati126 <erafebi535@students.unnes.ac.id>
2025-05-28 14:42:24 +0700

    Update App.py

--- App.py
+++ App.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="App.py")
 st.title("Hello, World!")
 st.write("Selamat datang di aplikasi Streamlit pertamamu!")
+
+name = st.text_input("Masukkan nama Anda:")
+if name:
+    st.write(f"Halo, {name}!")
